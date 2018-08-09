9754d6927ae094530d0d4ee6af9c5a85b55a755a
yuzt <yuztmt@126.com>
2018-08-09 17:34:21 +0800

    learning springboot and more

--- src/main/java/com/example/learn/swagger2/SwaggerDemoController.java
+++ src/main/java/com/example/learn/swagger2/SwaggerDemoController.java
@@ -7,8 +7,6 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
